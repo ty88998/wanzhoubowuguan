a57e68cf166b8c3646f54e7e96205737542ca917
ty <ty88998@qq.com>
2020-11-13 17:27:53 +0800

    202011131727代码进度上传

--- pages/topicShow/topicShow.js
+++ pages/topicShow/topicShow.js
@@ -23,13 +23,12 @@ Page({
   async onLoad(options) {
     // const museumInfo = await getMuseumsInfo()
     // this.setData({ museumInfo })
-
     this._getIndexData(options.recno)
   },
 
   // 获取展览信息
   async _getIndexData(recno) {
-    let {collectionList, currentItem} = this.data
+    let { collectionList, currentItem } = this.data
     const touristNo = appInst.globalData.touristNo;
     // 加载场景下文物
     getSceneCollect({ sceneNo: recno }).then(res => {
@@ -56,6 +55,7 @@ Page({
    */
   swiperChange(event) {
     const { current } = event.detail
+    console.log(current)
     this._setCurrentData(current)
     this.setData({
       currentItem: current
@@ -70,7 +70,32 @@ Page({
   _setCurrentData(current) {
     const { collectionList } = this.data
     const collectionItem = collectionList[current]
-      // contentBody更新
+    // contentBody更新
     this.setData({ collectionItem })
+  },
+  /**
+ * @param 箭头切换图片
+ */
+  //上一张
+  preCollection() {
+    const { currentItem,collectionList } = this.data;
+    if(currentItem===0){
+      this._setCurrentData(collectionList.length-1)
+      this.setData({currentItem:collectionList.length-1})
+    }else{
+      this._setCurrentData(currentItem-1)
+      this.setData({currentItem:currentItem-1})
+    }
+  },
+  //下一张
+  nextCollection(){
+    const { currentItem,collectionList } = this.data;
+    if(currentItem===collectionList.length-1){
+      this._setCurrentData(0)
+      this.setData({currentItem:0})
+    }else{
+      this._setCurrentData(currentItem+1)
+      this.setData({currentItem:currentItem+1})
+    }
   }
 })
