9ea8b570f549966958ff63ce9b618ab7e6ea527a
ty <ty88998@qq.com>
2020-11-24 09:46:08 +0800

    202011240945PC电脑代码备份

--- pages/three/three.js
+++ pages/three/three.js
@@ -1,44 +1,20 @@
 // pages/Three/Three.js
 import { getCollection, toProjectDetail } from '../../api/smallProgram'
-let innerAudioContext = {};
 Page({
   data: {
-    // innerAudioContext: {},
-    playStatus: false,
     src:''
   },
   onLoad: async function (options) {
-    const { recno, name } = options
-    innerAudioContext = wx.createInnerAudioContext()
-    wx.setNavigationBarTitle({ title: name })
-    const url = `${App.baseApi.Prod}SmallProgram/toCollection3D.do?recNo=${recno}`
-    this.setData({ src: url });
-    const collection = await getCollection({ recNo: recno })
-    this.setData({collection})
-    let that = this
-    setTimeout(function(){
-      innerAudioContext.src = collection.mp3Url
-      that.data.playStatus ? innerAudioContext.pause() : innerAudioContext.play()
-      that.setData({ playStatus: !that.data.playStatus })
-    }, 2500)
-    innerAudioContext.onEnded(function(){
-      this.setData({ playStatus: !this.data.playStatus })
-      innerAudioContext.stop()
-      innerAudioContext.destroy()
+    const { recno, name } = options;
+    wx.setNavigationBarTitle({ title: name });
+    //3d图形路径
+    const url = `${App.baseApi.Prod}SmallProgram/toCollection3D.do?recNo=${recno}`;
+    const collection = await getCollection({ recNo: recno });
+    //地址拼接--传值到web-view页面
+    this.setData({
+      src:url + "&" + collection.mp3Url
     })
   },
-
-  onUnload() {
-    this._leaveAndStopMp3()
-  },
-  /** 离开页面停止音乐播放 */
-  _leaveAndStopMp3() {
-    if (this.data.playStatus) {
-      innerAudioContext.stop()
-      innerAudioContext.destroy()
-    }
-  },
-
   back: function () {
     wx.navigateBack();
   }
